101c00a788c71eb50dc3c5aa4ab0ba4ad4b88307
William Tang <william.tang4@nio.com>
2024-03-22 10:18:36 +0800

    config: Add build script and patches to make it work on macOS

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+mvn -pl xxl-job-admin clean install
+mv xxl-job-admin/target/xxl-job-admin-2.4.1-SNAPSHOT.jar .
--- doc/db/tables_xxl_job.sql
+++ doc/db/tables_xxl_job.sql
@@ -2,8 +2,8 @@
 # XXL-JOB v2.4.1-SNAPSHOT
 # Copyright (c) 2015-present, xuxueli.
 
-CREATE database if NOT EXISTS `xxl_job` default character set utf8mb4 collate utf8mb4_unicode_ci;
-use `xxl_job`;
+CREATE database if NOT EXISTS `xxl` default character set utf8mb4 collate utf8mb4_unicode_ci;
+use `xxl`;
 
 SET NAMES utf8mb4;
 
